00af26010c5229094c9df0efd1463bb6f8d61ee3
Роман Носов <romannosov_2001@mail.ru>
2023-05-16 17:15:29 +0300

    Initial commit

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer>{
+    protected Random random;
+
+    public Randoms(int min, int max) {
+        //...
+    }
+
+    //...
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
