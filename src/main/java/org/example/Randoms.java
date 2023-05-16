2ad80df621840190f51af5633362e1d66886c198
Роман Носов <romannosov_2001@mail.ru>
2023-05-16 19:48:32 +0300

    1.0

--- src/main/java/org/example/Randoms.java
+++ src/main/java/org/example/Randoms.java
@@ -5,15 +5,27 @@ import java.util.Random;
 
 public class Randoms implements Iterable<Integer>{
     protected Random random;
-
+    protected int min;
+    protected int max;
     public Randoms(int min, int max) {
-        //...
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
     }
-
-    //...
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt((max - min) + 1) + min;
+            }
+        };
     }
 }
